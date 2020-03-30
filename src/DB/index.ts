0a1c24ff239de6680de1ec0f4c8c5d5445a638ac
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2020-03-29 20:37:04 -0600

    fix db for heroku

--- src/DB/index.ts
+++ src/DB/index.ts
@@ -8,15 +8,13 @@ const connect_db = async () => {
       process.env.DEV == "true"
         ? await mongoose.connect(`mongodb://127.0.0.1:27017/wallet`, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false
+            useUnifiedTopology: true
           })
         : await mongoose.connect(
             `mongodb+srv://ECOLOTE:${process.env.DB_PASS}@cluster0-u30oq.mongodb.net/wallet`,
             {
               useNewUrlParser: true,
-              useUnifiedTopology: true,
-              useFindAndModify: false
+              useUnifiedTopology: true
             }
           );
 
