4d7295a3587204b5ab662b92ca8387913f75d2a8
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2020-03-29 20:21:08 -0600

    fix db connection

--- src/DB/index.ts
+++ src/DB/index.ts
@@ -4,20 +4,21 @@ const connect_db = async () => {
   try {
     mongoose.set("useCreateIndex", true);
 
-    let db = process.env.DEV
-      ? await mongoose.connect(`mongodb://127.0.0.1:27017/wallet`, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-          useFindAndModify: false
-        })
-      : await mongoose.connect(
-          `mongodb+srv://ECOLOTE:${process.env.DB_PASS}@cluster0-u30oq.mongodb.net/wallet`,
-          {
+    let db =
+      process.env.DEV == "true"
+        ? await mongoose.connect(`mongodb://127.0.0.1:27017/wallet`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
-          }
-        );
+          })
+        : await mongoose.connect(
+            `mongodb+srv://ECOLOTE:${process.env.DB_PASS}@cluster0-u30oq.mongodb.net/wallet`,
+            {
+              useNewUrlParser: true,
+              useUnifiedTopology: true,
+              useFindAndModify: false
+            }
+          );
 
     return Promise.resolve({ msg: "Data base conected" });
   } catch (error) {
